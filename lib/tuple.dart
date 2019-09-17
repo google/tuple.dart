093e6eca700d8f7ea5be2ebd75ed212b42d0da0a
zubair <zubairehman.work@gmail.com>
2019-09-17 12:33:56 +0500

    - added support for eleventh tuple

--- lib/tuple.dart
+++ lib/tuple.dart
@@ -814,7 +814,7 @@ class Tuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> {
 
   /// Create a new tuple value with the specified list [items].
   factory Tuple10.fromList(List items) {
-    if (items.length != 9) {
+    if (items.length != 10) {
       throw ArgumentError('items must have length 9');
     }
 
@@ -895,9 +895,9 @@ class Tuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> {
   ///
   /// The elements are in item order. The list is variable-length
   /// if [growable] is true.
-  List toList({bool growable = false}) =>
-      List.from([item1, item2, item3, item4, item5, item6, item7, item8, item9, item10],
-          growable: growable);
+  List toList({bool growable = false}) => List.from(
+      [item1, item2, item3, item4, item5, item6, item7, item8, item9, item10],
+      growable: growable);
 
   @override
   String toString() =>
@@ -929,5 +929,183 @@ class Tuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> {
         item8.hashCode,
         item9.hashCode,
         item10.hashCode
+      ]);
+}
+
+/// Represents a 11-tuple.
+class Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> {
+  /// Returns the first item of the tuple
+  final T1 item1;
+
+  /// Returns the second item of the tuple
+  final T2 item2;
+
+  /// Returns the third item of the tuple
+  final T3 item3;
+
+  /// Returns the fourth item of the tuple
+  final T4 item4;
+
+  /// Returns the fifth item of the tuple
+  final T5 item5;
+
+  /// Returns the sixth item of the tuple
+  final T6 item6;
+
+  /// Returns the seventh item of the tuple
+  final T7 item7;
+
+  /// Returns the eight item of the tuple
+  final T8 item8;
+
+  /// Returns the ninth item of the tuple
+  final T9 item9;
+
+  /// Returns the tenth item of the tuple
+  final T10 item10;
+
+  /// Returns the eleventh item of the tuple
+  final T11 item11;
+
+  /// Creates a new tuple value with the specified items.
+  const Tuple11(this.item1, this.item2, this.item3, this.item4, this.item5,
+      this.item6, this.item7, this.item8, this.item9, this.item10, this.item11);
+
+  /// Create a new tuple value with the specified list [items].
+  factory Tuple11.fromList(List items) {
+    if (items.length != 11) {
+      throw ArgumentError('items must have length 9');
+    }
+
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(
+        items[0] as T1,
+        items[1] as T2,
+        items[2] as T3,
+        items[3] as T4,
+        items[4] as T5,
+        items[5] as T6,
+        items[6] as T7,
+        items[7] as T8,
+        items[8] as T9,
+        items[9] as T10,
+        items[10] as T11);
+  }
+
+  /// Returns a tuple with the first item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem1(T1 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(v, item2,
+        item3, item4, item5, item6, item7, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the second item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem2(T2 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, v,
+        item3, item4, item5, item6, item7, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the third item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem3(T3 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        v, item4, item5, item6, item7, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the fourth item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem4(T4 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, v, item5, item6, item7, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the fifth item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem5(T5 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, v, item6, item7, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the sixth item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem6(T6 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, item5, v, item7, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the seventh item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem7(T7 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, item5, item6, v, item8, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the eigth item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem8(T8 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, item5, item6, item7, v, item9, item10, item11);
+  }
+
+  /// Returns a tuple with the ninth item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem9(T9 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, item5, item6, item7, item8, v, item10, item11);
+  }
+
+  /// Returns a tuple with the tenth item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem10(T10 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, item5, item6, item7, item8, item9, v, item11);
+  }
+
+  /// Returns a tuple with the eleventh item set to the specified value.
+  Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> withItem11(T11 v) {
+    return Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(item1, item2,
+        item3, item4, item5, item6, item7, item8, item9, item10, v);
+  }
+
+  /// Creates a [List] containing the items of this [Tuple5].
+  ///
+  /// The elements are in item order. The list is variable-length
+  /// if [growable] is true.
+  List toList({bool growable = false}) => List.from([
+        item1,
+        item2,
+        item3,
+        item4,
+        item5,
+        item6,
+        item7,
+        item8,
+        item9,
+        item10,
+        item11
+      ], growable: growable);
+
+  @override
+  String toString() =>
+      '[$item1, $item2, $item3, $item4, $item5, $item6, $item7, $item8, $item9, $item10, $item11]';
+
+  @override
+  bool operator ==(other) =>
+      other is Tuple11 &&
+      other.item1 == item1 &&
+      other.item2 == item2 &&
+      other.item3 == item3 &&
+      other.item4 == item4 &&
+      other.item5 == item5 &&
+      other.item6 == item6 &&
+      other.item7 == item7 &&
+      other.item8 == item8 &&
+      other.item9 == item9 &&
+      other.item10 == item10 &&
+      other.item11 == item11;
+
+  @override
+  int get hashCode => hashObjects([
+        item1.hashCode,
+        item2.hashCode,
+        item3.hashCode,
+        item4.hashCode,
+        item5.hashCode,
+        item6.hashCode,
+        item7.hashCode,
+        item8.hashCode,
+        item9.hashCode,
+        item10.hashCode,
+        item1.hashCode
       ]);
 }
