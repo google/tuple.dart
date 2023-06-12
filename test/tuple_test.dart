a276f1c566a1c1d97ad1db96730d030689a92e68
Devon Carew <devoncarew@google.com>
2023-06-12 08:46:03 -0700

    update the status of this package in the readme (#46)
    * update the status of this package in the readme
    
    * update code sample

--- test/tuple_test.dart
+++ test/tuple_test.dart
@@ -2,8 +2,6 @@
 // file for details. All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: prefer_const_constructors
-
 import 'package:test/test.dart';
 import 'package:tuple/tuple.dart';
 
